5372f642e672a3c50aee724a130014c5f83df800
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-07-11 16:28:33 +0700

    Move to basic dialog for settings

--- app/src/main/java/com/andriawan/hydrationtracker/ui/components/DialogEditValue.kt
+++ app/src/main/java/com/andriawan/hydrationtracker/ui/components/DialogEditValue.kt
@@ -1,14 +1,20 @@
 package com.andriawan.hydrationtracker.ui.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.andriawan.hydrationtracker.theme.HydrationTrackerTheme
 
 @Composable
 fun DialogEditValue(
@@ -19,17 +25,22 @@ fun DialogEditValue(
 ) {
     var newValue by remember { mutableStateOf(value) }
 
-    AlertDialog(
-        title = {
-            Text(text = title, style = MaterialTheme.typography.h3)
-        },
-        onDismissRequest = {
-            onDismiss()
-        },
-        text = {
-            Column {
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Card(
+            shape = MaterialTheme.shapes.small
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(12.dp),
+                verticalArrangement = Arrangement.spacedBy(18.dp)
+            ) {
+                Text(
+                    text = title, style = MaterialTheme.typography.h3
+                )
+
                 OutlinedTextField(
-                    modifier = Modifier.padding(top = 12.dp),
                     value = newValue,
                     onValueChange = {
                         newValue = it
@@ -39,28 +50,55 @@ fun DialogEditValue(
                     },
                     keyboardOptions = KeyboardOptions.Default.copy(
                         keyboardType = KeyboardType.Number
-                    )
+                    ),
+                    textStyle = MaterialTheme.typography.body1,
+                    modifier = Modifier.fillMaxWidth()
                 )
-            }
-        },
-        confirmButton = {
-            Button(
-                onClick = {
-                    onSubmit(newValue)
+
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = { onDismiss() },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Red
+                        )
+                    ) {
+                        Text(
+                            text = "Cancel",
+                            style = MaterialTheme.typography.body1.copy(
+                                color = Color.White,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    Button(
+                        onClick = {
+                            onSubmit(newValue)
+                        }
+                    ) {
+                        Text(
+                            text = "Save",
+                            style = MaterialTheme.typography.body1.copy(
+                                color = Color.White,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+                    }
                 }
-            ) {
-                Text(text = "Save")
-            }
-        },
-        dismissButton = {
-            Button(
-                onClick = { onDismiss() },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Red
-                )
-            ) {
-                Text(text = "Cancel")
             }
         }
-    )
+    }
+}
+
+@Preview
+@Composable
+fun DialogEditValuePreview() {
+    HydrationTrackerTheme {
+        DialogEditValue(title = "Hello", value = "100", onSubmit = {}, onDismiss = {})
+    }
 }
